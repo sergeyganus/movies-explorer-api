1b4d36d0a859422da215f8a9ca7195039dcb6b5f
Sergey Ganus <sergey.ganus@gmail.com>
2023-08-27 18:50:31 +0300

    feat: добавить limiter и helmet

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const cors = require('./middlewares/cors');
+const limiter = require('./middlewares/limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
 const router = require('./routes/index');
@@ -17,6 +19,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb')
   .catch((err) => console.log(`Произошла ошибка: ${err.message}`));
 
 app.use(cors);
+app.use(limiter);
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
